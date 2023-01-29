77cf499bd5cd39c0ce350449e79e0221195ca96e
John Howard <joh.d.howard@protonmail.com>
2023-01-29 15:35:30 +0100

    sort out the FRR side

--- PC-interfaces.sh
+++ PC-interfaces.sh
@@ -5,11 +5,11 @@ sudo docker exec -d clab-wg-dci-evpn-PC1 ip route add 192.168.0.0/16 via 192.168
 sudo docker exec -d clab-wg-dci-evpn-PC1 ip route add 10.10.10.0/24 via 192.168.11.1 dev eth1
 
 sudo docker exec -d clab-wg-dci-evpn-PC2 ip link set eth1 up
-sudo docker exec -d clab-wg-dci-evpn-PC2 ip addr add 192.168.12.2/24 dev eth1
-sudo docker exec -d clab-wg-dci-evpn-PC2 ip route add 192.168.0.0/16 via 192.168.12.1 dev eth1
-sudo docker exec -d clab-wg-dci-evpn-PC2 ip route add 10.10.10.0/24 via 192.168.12.1 dev eth1
+sudo docker exec -d clab-wg-dci-evpn-PC2 ip addr add 192.168.22.2/24 dev eth1
+sudo docker exec -d clab-wg-dci-evpn-PC2 ip route add 192.168.0.0/16 via 192.168.22.1 dev eth1
+sudo docker exec -d clab-wg-dci-evpn-PC2 ip route add 10.10.10.0/24 via 192.168.22.1 dev eth1
 
 sudo docker exec -d clab-wg-dci-evpn-PC3 ip link set eth1 up
-sudo docker exec -d clab-wg-dci-evpn-PC3 ip addr add 192.168.13.2/24 dev eth1
-sudo docker exec -d clab-wg-dci-evpn-PC3 ip route add 192.168.0.0/16 via 192.168.13.1 dev eth1
-sudo docker exec -d clab-wg-dci-evpn-PC3 ip route add 10.10.10.0/24 via 192.168.13.1 dev eth1
+sudo docker exec -d clab-wg-dci-evpn-PC3 ip addr add 192.168.33.2/24 dev eth1
+sudo docker exec -d clab-wg-dci-evpn-PC3 ip route add 192.168.0.0/16 via 192.168.33.1 dev eth1
+sudo docker exec -d clab-wg-dci-evpn-PC3 ip route add 10.10.10.0/24 via 192.168.33.1 dev eth1
