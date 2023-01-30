779d2cbd08c0043094f2be9dbedae5d16fb42c4b
John Howard <john.howard@proton.ch>
2023-01-30 08:53:37 +0100

    setup overlay IPs in PCs

--- PC-interfaces.sh
+++ PC-interfaces.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 sudo docker exec -d clab-wg-dci-evpn-PC1 ip link set eth1 up
-sudo docker exec -d clab-wg-dci-evpn-PC1 ip addr add 192.168.11.2/24 dev eth1
-sudo docker exec -d clab-wg-dci-evpn-PC1 ip route add 192.168.0.0/16 via 192.168.11.1 dev eth1
-sudo docker exec -d clab-wg-dci-evpn-PC1 ip route add 10.10.10.0/24 via 192.168.11.1 dev eth1
+sudo docker exec -d clab-wg-dci-evpn-PC1 ip addr add 172.16.0.1/24 dev eth1
 
 sudo docker exec -d clab-wg-dci-evpn-PC2 ip link set eth1 up
-sudo docker exec -d clab-wg-dci-evpn-PC2 ip addr add 192.168.22.2/24 dev eth1
-sudo docker exec -d clab-wg-dci-evpn-PC2 ip route add 192.168.0.0/16 via 192.168.22.1 dev eth1
-sudo docker exec -d clab-wg-dci-evpn-PC2 ip route add 10.10.10.0/24 via 192.168.22.1 dev eth1
+sudo docker exec -d clab-wg-dci-evpn-PC2 ip addr add 172.16.0.2/24 dev eth1
 
 sudo docker exec -d clab-wg-dci-evpn-PC3 ip link set eth1 up
-sudo docker exec -d clab-wg-dci-evpn-PC3 ip addr add 192.168.33.2/24 dev eth1
-sudo docker exec -d clab-wg-dci-evpn-PC3 ip route add 192.168.0.0/16 via 192.168.33.1 dev eth1
-sudo docker exec -d clab-wg-dci-evpn-PC3 ip route add 10.10.10.0/24 via 192.168.33.1 dev eth1
+sudo docker exec -d clab-wg-dci-evpn-PC3 ip addr add 172.16.0.3/24 dev eth1
