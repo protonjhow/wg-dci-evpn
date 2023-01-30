453627adf2462899b86da21b0417ffd97ef69331
John Howard <john.howard@proton.ch>
2023-01-30 09:12:32 +0100

    move wg address from config to ip a

--- FRR-config.sh
+++ FRR-config.sh
@@ -2,6 +2,10 @@
 ## dci-router1
 # install wireguard
 sudo docker exec -d clab-wg-dci-evpn-dci-router1 apk add wireguard-tools
+# setup wireguard port
+sudo docker exec -d clab-wg-dci-evpn-dci-router1 ip link add dev wg0 type wireguard
+sudo docker exec -d clab-wg-dci-evpn-dci-router1 ip address add wg0 192.168.0.1/24
+sudo docker exec -d clab-wg-dci-evpn-dci-router1 wg setconf wg0 /etc/wg.conf
 
 # vtep loopback
 sudo docker exec -d clab-wg-dci-evpn-dci-router1 ip addr add 100.127.0.1/32 dev lo
@@ -25,6 +29,13 @@ sudo docker exec -d clab-wg-dci-evpn-dci-router1 ip link set eth2.110 master br1
 sudo docker exec -d clab-wg-dci-evpn-dci-router1 ip link set eth2.110 up
 
 ## dci-router2
+# install wireguard
+sudo docker exec -d clab-wg-dci-evpn-dci-router2 apk add wireguard-tools
+# setup wireguard port
+sudo docker exec -d clab-wg-dci-evpn-dci-router2 ip link add dev wg0 type wireguard
+sudo docker exec -d clab-wg-dci-evpn-dci-router2 ip address add wg0 192.168.0.2/24
+sudo docker exec -d clab-wg-dci-evpn-dci-router2 wg setconf wg0 /etc/wg.conf
+
 # vtep loopback
 sudo docker exec -d clab-wg-dci-evpn-dci-router2 ip addr add 100.127.0.2/32 dev lo
 
@@ -47,6 +58,13 @@ sudo docker exec -d clab-wg-dci-evpn-dci-router2 ip link set eth2.110 master br1
 sudo docker exec -d clab-wg-dci-evpn-dci-router2 ip link set eth2.110 up
 
 ## dci-router3
+# install wireguard
+sudo docker exec -d clab-wg-dci-evpn-dci-router3 apk add wireguard-tools
+# setup wireguard port
+sudo docker exec -d clab-wg-dci-evpn-dci-router3 ip link add dev wg0 type wireguard
+sudo docker exec -d clab-wg-dci-evpn-dci-router3 ip address add wg0 192.168.0.3/24
+sudo docker exec -d clab-wg-dci-evpn-dci-router3 wg setconf wg0 /etc/wg.conf
+
 # vtep loopback
 sudo docker exec -d clab-wg-dci-evpn-dci-router3 ip addr add 100.127.0.3/32 dev lo
 
